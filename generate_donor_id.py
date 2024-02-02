c71a369ecdab540ab3736b4419204eee671ef569
Jamie Zieziula <jamie.m.dick@gmail.com>
2024-02-02 14:03:26 -0500

    deployment

--- generate_donor_id.py
+++ generate_donor_id.py
@@ -5,11 +5,15 @@ from prefect.blocks.system import JSON
 
 
 @flow
-def generate_donor_id():
+def generate_donor_id(
+    number_of_ids: int = 1
+):
+    # Load existing donor IDs
     donor_ids = JSON.load("donor-ids").value
 
     complete = False
 
+    # Generate Donor ID and ensure it does not already exist
     while complete == False:
         donor_id = random.randint(1000, 9999)
 
